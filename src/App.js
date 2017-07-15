84e808d4f63322c289fda4fad4c821b58a8e735d
eli9000 <eli9000@gmail.com>
2017-07-15 04:10:31 -0600

    Adding images & SCSS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 import Header from './components/Header.js';
 import Main from './components/Main.js';
+// import About from './components/About.js';
 import Footer from './components/Footer.js';
+
+// import './components/header-shrink.js';
 
 import './styles/App.css';
 
@@ -11,7 +14,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Main />
+        <Main className="Main" />
+        <Main className="About" />
+        <Main className="Issues" />
+        <Main className="Plan" />
+        <Main className="Involve" />
         <Footer />
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,8 @@ import '../main.scss';
 class Main extends Component {
   render() {
     return (
-      <div className="Main">
-        <h1>Main</h1>
+      <div className={this.props.className}>
+        <h1>{this.props.className}</h1>
       </div>
     );
   }
